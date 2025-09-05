862b427f8b1fc86d2abcc538b408c4d4696ebdf7
Siddharth Lala <siddharthlala13@gmail.com>
2025-09-05 10:51:20 +0100

    Client-Server model
    Client - Server model built for communication using TCP socket programming

---
+++ project 2 code/Readme.txt
@@ -0,0 +1,16 @@
+Please follow these instructions:
+
+Libraries used: 
+Socket
+Threading
+Sys
+Os
+
+
+1. First Run the server.py file otherwise client will not be able to run and establish connection with the server. 
+2. then run client1.py client2.py client3.py [all in different terminals][you can create more clients but copying the client code into a new file and running that on another terminal like the previous steps]
+
+After the connection of the clients server is established, the communication happens between the clients and the server gets a receipt of the communication. 
+
+Now send messages between the clients. And also view the server terminal. 
+Thanks!
---
+++ project 2 code/client3.py
@@ -0,0 +1,49 @@
+import socket
+import threading
+import sys
+import os
+
+#Wait for incoming data from server
+#.decode is used to turn the message in bytes to a string
+def receive(socket, signal):
+    while signal:
+        try:
+            data = socket.recv(32)
+            newData = str(data.decode("utf-8"))
+            if(newData == 'exit'):
+                 
+                break
+                 
+            else:    
+                print("\n"+newData)
+        except:
+            print("You have been disconnected from the server")
+            signal = False
+            sys.exit(0)
+
+#Get host and port
+host = "localhost"
+port = 6666
+
+
+#Attempt connection to server
+try:
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    sock.connect((host, port))
+    print("Connected to Server.....\n")
+except:
+    print("Could not make a connection to the server")
+    input("Press enter to quit")
+    sys.exit(0)
+
+#Create new thread to wait for data
+receiveThread = threading.Thread(target = receive, args = (sock, True))
+receiveThread.start()
+
+#Send data to server
+#str.encode is used to turn the string message into bytes so it can be sent across the network
+while True:
+    message = input()
+    if(message == 'exit'):
+        break
+    sock.sendall(str.encode(message))
